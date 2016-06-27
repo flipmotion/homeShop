9704dcacb61a61e35241b7c70db3a0852da4fc2e
igor <flipmotions@gmail.com>
2016-06-28 01:05:03 +0300

    UP
    1-5 fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,63 @@
 $(document).ready(function () {
+  $(".fancybox").fancybox();
 
-  function slider(sliderName, count) {
+  var cnt2 = {
+    0: {
+      items: 1
+    },
+    480: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    992: {
+      items: 2
+    }
+  };
+  var cnt3 = {
+    0: {
+      items: 1
+    },
+    480: {
+      items: 1
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 2
+    }
+  };
+  var cnt7 = {
+    0: {
+      items: 3
+    },
+    480: {
+      items: 3
+    },
+    768: {
+      items: 7
+    },
+    992: {
+      items: 9
+    }
+  };
+  function slider(sliderName, count, resp) {
     $(sliderName).owlCarousel({
       items: count,
       loop: true,
       nav: true,
       pagination: false,
-      navText: ["<i class='my-arrow-left'></i>", "<i class='my-arrow-right'></i>"]
+      navText: ["<i class='my-arrow-left'></i>", "<i class='my-arrow-right'></i>"],
+      responsive: resp
     });
   };
   slider('[data-slider="1"]', 1);
-  slider('[data-slider="9"]', 9);
-  slider('[data-slider="2"]', 2);
-  slider('[data-slider="3"]', 3);
+  slider('[data-slider="9"]', 9, cnt7);
+  slider('[data-slider="2"]', 2, cnt3);
+  slider('[data-slider="3"]', 3, cnt2);
+  slider('[data-slider="2-2"]', 2, cnt2);
 
   $('[data-slider="main"]').owlCarousel({
     items: 1,
