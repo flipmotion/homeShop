9d4c4c1b13a935c1074a3d42cff866a72ba6ce2c
igor <flipmotions@gmail.com>
2016-06-29 18:32:16 +0300

    UP
    filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@
 
 $(document).ready(function () {
   $(".fancybox").fancybox();
-
   var cnt2 = {
     0: {
       items: 1
@@ -25,7 +24,7 @@ $(document).ready(function () {
       items: 1
     },
     768: {
-      items: 3
+      items: 2
     },
     992: {
       items: 2
@@ -86,6 +85,52 @@ $(document).ready(function () {
       send();
     }
   });
+  var sliders = document.getElementsByClassName('range-sliders');
+
+  for (var i = 0; i < sliders.length; i++) {
+
+    noUiSlider.create(sliders[i], {
+      start: [4000, 8000],
+      connect: true,
+      range: {
+        'min': [2000],
+        'max': [10000]
+      }
+    });
+
+    sliders[i].noUiSlider.on('slide', addValues);
+  }
+
+  function addValues() {
+    var allValues = [];
+
+    for (var i = 0; i < sliders.length; i++) {
+      console.log(allValues.push(sliders[i].noUiSlider.get()));
+    };
+
+    console.log(allValues);
+  }
+
+  $(".tags").select2({
+    theme: 'bootstrap',
+    minimumResultsForSearch: Infinity,
+    placeholder: 'Выберите тип'
+  }).on('change', function () {
+    var $selected = $(this).find('option:selected');
+    var $container = $(this).siblings('.tags-container');
+
+    var $list = $('<ul>');
+    $selected.each(function (k, v) {
+      var $li = $('<li class="tag-selected"><a class="destroy-tag-selected">×</a>' + $(v).text() + '</li>');
+      $li.children('a.destroy-tag-selected').off('click.select2-copy').on('click.select2-copy', function (e) {
+        var $opt = $(this).data('select2-opt');
+        $opt.attr('selected', false);
+        $opt.parents('select').trigger('change');
+      }).data('select2-opt', $(v));
+      $list.append($li);
+    });
+    $container.html('').append($list);
+  }).trigger('change');
 });
 function send() {
   var form = $('[data-form="send"]');
